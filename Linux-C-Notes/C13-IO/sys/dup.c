d4bbf67d880d6419135ac180885f94d4136525a5
tianxiang zhou <tianxiangzhou@mail.nwpu.edu.cn>
2025-04-24 09:41:15 +0800

    Update dup.c

--- Linux-C-Notes/C13-IO/sys/dup.c
+++ Linux-C-Notes/C13-IO/sys/dup.c
@@ -7,29 +7,29 @@
 
 int main(int argc, char **argv)
 {
-    int fd;
+  int fd;
 
-    /***
-     * bug:
-     * 1. 假如默认没有1，这里有bug。
-     * 2. 假如这个非原子操作的操作的过程，有可能被别的进程影响。
-     */
-    fd = open(FNAME, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd < 0)
-    {
-        perror("open");
-        exit(1);
-    }
+  /***
+   * bug:
+   * 1. 假如默认没有1，这里有bug。
+   * 2. 假如这个非原子操作的操作的过程，有可能被别的进程影响。
+   */
+  fd = open(FNAME, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+  if (fd < 0)
+  {
+    perror("open");
+    exit(1);
+  }
 
-    // close(1);
-    // dup(fd);
-    dup2(fd, 1);
-    // 使用 dup2 原子操作
-    // 且dup2如果两个参数相等，不会做任何事
+  // close(1);
+  // dup(fd);
+  dup2(fd, 1);
+  // 使用 dup2 原子操作
+  // 且dup2如果两个参数相等，不会做任何事
 
-    // !!!
-    if (1 != fd)
-        close(fd);
+  // !!!
+  if (1 != fd)
+    close(fd);
 
 #if 0
     close(1);
@@ -42,10 +42,10 @@ int main(int argc, char **argv)
     }
 #endif
 
-    /********************************************************************/
-    puts("hello!");
+  /********************************************************************/
+  puts("hello!");
 
-    // TODO: 将sdtout回到终端
+  // TODO: 将sdtout回到终端
 
-    exit(0);
+  exit(0);
 }
